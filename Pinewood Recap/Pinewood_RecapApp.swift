9b850c0689ea1e7bf3839d62fa2557cec29bd6c3
Adam Xu <contact@adamxu.net>
2026-05-15 12:55:59 -0700

    Initial Commit

---
+++ Pinewood Recap/Pinewood_RecapApp.swift
@@ -0,0 +1,17 @@
+//
+//  Pinewood_RecapApp.swift
+//  Pinewood Recap
+//
+//  Created by Adam Xu on 5/15/26.
+//
+
+import SwiftUI
+
+@main
+struct Pinewood_RecapApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
